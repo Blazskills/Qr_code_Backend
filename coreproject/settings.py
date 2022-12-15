26e6358bdcd0fded8d6a6c632fc8766c67cac14e
Blazskills <ilesanmiisaac@gmail.com>
2022-12-15 06:50:36 +0100

    update static files

--- coreproject/settings.py
+++ coreproject/settings.py
@@ -211,6 +211,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
